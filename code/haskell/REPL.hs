334cc5a61826aea1e84121dddbe843bfd910e3a4
AntiX <kumoyuki@gmail.com>
2025-07-09 10:43:05 +0100

    thinking out loud about DS for macros

--- code/haskell/REPL.hs
+++ code/haskell/REPL.hs
@@ -6,16 +6,48 @@ import Prelude hiding (catch)
 
 import System.Console.Haskeline as HL
 
+
+-- one of the points of this code is to figure out WTF the type should be
+-- for EVAL in a lambda calculus with macros. Remember that the existence
+-- of "define" pretty much requires that EVAL implement a State monad, so
+-- what does it even mean to have a LC w/macros?
+
+-- more specifically, how do we encode an LC interpreter in a minimalist
+-- LC? Note that we are actually leaning into Scheme as our model (b/c the
+-- paper), so we may want to allow certain syntactic sugars like letrec
+-- 
+
+data Datum = Nil | Cons Datum Datum | Symbol String
+
+data PD = Exit | Malformed String | SExp Datum
+
+
+-- expand later
+parse :: Maybe String -> PD
+parse Nothing = Exit
+parse (Just buffer) = SExp Nil
+
+--render :: Datum -> IO ()
+render d = do
+  outputStrLn "rendered"
+
+-- main IO loop
 main :: IO ()
 main = HL.runInputT HL.defaultSettings mloop
 
 mloop = do
   buffer <- HL.getInputLine "hello sailor."
-  case buffer of
-    (Just answer) -> do
-       outputStrLn ("Wow. Really? \"" ++ answer ++ "\"?")
-       mloop
-    Nothing -> outputStrLn "Be that way..."
+  pdatum <- parse buffer
+  case pdatum of
+    (SExp datum) -> do
+      render datum
+      mloop
+
+    (Malformed text) -> do
+      outputStrLn ("Wow. Really? \"" ++ text ++ "\"?")
+      mloop
+                
+    Exit -> outputStrLn "Be that way..."
 
 
 
